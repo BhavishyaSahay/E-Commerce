98e982da3dd5bf4378f33bdf8a0dc7c7c708a42e
Bhavishya Sahay <sahaybhavishya@gmail.com>
2026-01-29 10:49:37 +0530

    Initial commit of project setup

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('E-Commerce API is running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
